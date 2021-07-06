80d9ff04d921234494fcd2310223fc05ef60a583
omelchenkoaleks <omelchenkoaleks@gmail.com>
2021-07-06 15:18:06 +0300

    custom toolbar

--- app/src/main/java/com/omelchenkoaleks/rememberenglishwords/MainActivity.kt
+++ app/src/main/java/com/omelchenkoaleks/rememberenglishwords/MainActivity.kt
@@ -2,6 +2,7 @@ package com.omelchenkoaleks.rememberenglishwords
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import com.omelchenkoaleks.rememberenglishwords.databinding.ActivityMainBinding
@@ -11,6 +12,7 @@ class MainActivity : AppCompatActivity() {
     private var _binding: ActivityMainBinding? = null
     private val binding get() = _binding!!
 
+    lateinit var toolbar: Toolbar
     lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,6 +21,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         APP_ACTIVITY = this
+
+        toolbar = binding.toolbar
+        setSupportActionBar(toolbar)
+        title = getString(R.string.app_name)
 
         navController = Navigation.findNavController(this, R.id.nav_host_fragment)
     }
--- app/src/main/java/com/omelchenkoaleks/rememberenglishwords/ui/AddWordFragment.kt
+++ app/src/main/java/com/omelchenkoaleks/rememberenglishwords/ui/AddWordFragment.kt
@@ -15,7 +15,7 @@ class AddWordFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentAddWordBinding.inflate(inflater, container, false)
         return binding.root
     }
